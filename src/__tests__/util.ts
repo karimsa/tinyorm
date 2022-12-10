460306cf9161c1f891423c142f385d1acca09275
Karim Alibhai <karim@alibhai.co>
2022-12-09 23:06:21 -0500

    add formatter for tests

--- src/__tests__/util.ts
+++ src/__tests__/util.ts
@@ -1,11 +1,18 @@
 import { FinalizedQuery } from "../queries";
+import { format } from "sql-formatter";
+
+const pretty = (text: string) => {
+	try {
+		return format(text, { language: "postgresql" });
+	} catch (err) {
+		throw new Error(`Failed to format: '${text}'`);
+	}
+};
 
 export function expectQuery(given: FinalizedQuery) {
 	return {
 		toEqual(expected: FinalizedQuery) {
-			expect(given.text.trim().replace(/\s+/g, " ")).toEqual(
-				expected.text.trim().replace(/\s+/g, " "),
-			);
+			expect(pretty(given.text)).toEqual(pretty(expected.text));
 			expect(given).toMatchObject({
 				text: given.text,
 				values: expected.values,
