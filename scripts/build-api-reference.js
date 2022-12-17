d883815e504311a6542649ad9d2863ad3e16d597
Karim Alibhai <karim@alibhai.co>
2022-12-17 17:49:51 -0500

    fix ci

--- scripts/build-api-reference.js
+++ scripts/build-api-reference.js
@@ -1,7 +1,7 @@
-#!/usr/bin/env zx
+#!/usr/bin/env node
 
-import * as fs from "fs";
-import * as path from "path";
+const fs = require("fs");
+const path = require("path");
 
 function flattenNode(node) {
 	if (node.kindString === "Reference") {
