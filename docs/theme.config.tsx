27a7880e1c40b41a35111e65dddb95e11c8d1175
Karim Alibhai <karim@alibhai.co>
2022-12-17 22:52:04 -0500

    fix double title

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -3,15 +3,6 @@ import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
 	logo: <span>TinyORM</span>,
-	head: () => {
-		const { title } = useConfig();
-
-		return (
-			<>
-				<title>{title} - TinyORM</title>
-			</>
-		);
-	},
 	project: {
 		link: "https://github.com/karimsa/tinyorm",
 	},
